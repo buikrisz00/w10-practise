86cc1fc256ebf83f538ef55967138389687e8e7b
Krisztian Bui <buikrisz00@gmail.com>
2022-03-24 09:57:42 +0100

    a

--- myApp.js
+++ myApp.js
@@ -4,6 +4,9 @@ const port = 9000;
 const fs = require("fs");
 /* const beers = require("./beers.json"); */
 
+// For css style
+app.use("/style", express.static(__dirname + "/style"));
+
 const data = fs.readFileSync("./beers.json", error => {
     if (error) {
         console.log(error);
@@ -14,17 +17,15 @@ const beers = JSON.parse(data);
 // For index.html
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
-})
+});
 
 // For beers.json
 app.get("/api/beers", (req, res) => {
     res.send(beers);
-})
+});
 
-// For css style
-app.use("/style", express.static(__dirname + "/style"))
 
 // For listening on port 9000
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
-})
+});
